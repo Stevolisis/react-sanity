20f8998e5fe0e606366857a16a75a4dd0b50b941
Steven Joseph <stevolisisjoseph@gmail.com>
2023-12-06 20:33:25 +0100

    all router,routes,route imported from react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import './index.css'
-import { Route, Routes } from 'react-router';
-import { HashRouter  } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Index from './pages';
 import Post from './pages/post';
 import AboutUs from './pages/about_us';
@@ -11,15 +10,15 @@ function App() {
 
   return (
     <>
-      <HashRouter >
+      <Router>
         <Routes>
-          <Route path="/" element={<Index />} exact/>
+          <Route path="/" element={<Index />}/>
           <Route path="/category/:id" element={<Category />}/>
           <Route path="/posts" element={<Posts />}/>
           <Route path="/post/:id" element={<Post />}/>
           <Route path="/about_us" element={<AboutUs />}/>
         </Routes>
-      </HashRouter >
+      </Router>
     </>
   )
 }
