85e0e3790d095e6c7d1cfdc90bdc6d7c026653d3
Steven Joseph <stevolisisjoseph@gmail.com>
2023-12-06 21:16:39 +0100

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: "0.0.0.0", // This makes the server accessible externally
+    port: 3000, // You can specify the port you want to use
+  },
   plugins: [react()],
-})
+});
