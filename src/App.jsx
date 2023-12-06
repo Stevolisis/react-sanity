64f51b0d246c81585c11e681fe1ecfb1c7e3a73b
Steven Joseph <stevolisisjoseph@gmail.com>
2023-12-06 20:40:48 +0100

    back to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './index.css'
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import { Route, Routes } from 'react-router';
+import { HashRouter } from 'react-router-dom';
 import Index from './pages';
 import Post from './pages/post';
 import AboutUs from './pages/about_us';
@@ -7,20 +8,18 @@ import Posts from './pages/posts';
 import Category from './pages/Category';
 
 function App() {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <>
-        <Route path="/" element={<Index />}/>
-        <Route path="/category/:id" element={<Category />}/>
-        <Route path="/posts" element={<Posts />}/>
-        <Route path="/post/:id" element={<Post />}/>
-        <Route path="/about_us" element={<AboutUs />}/>
-      </>
-    )
-  )
+
   return (
     <>
-      <RouterProvider router={router}/>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Index />}/>
+          <Route path="/category/:id" element={<Category />}/>
+          <Route path="/posts" element={<Posts />}/>
+          <Route path="/post/:id" element={<Post />}/>
+          <Route path="/about_us" element={<AboutUs />}/>
+        </Routes>
+      </HashRouter>
     </>
   )
 }
