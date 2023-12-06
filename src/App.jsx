c88b6bdcb65f714ae71512ea6235167f06609842
Steven Joseph <stevolisisjoseph@gmail.com>
2023-12-06 01:13:04 +0100

    category fixies

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import './index.css'
 import { Route, Routes } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
 import Index from './pages';
-import Category from './pages/category';
 import Post from './pages/post';
 import AboutUs from './pages/about_us';
 import Posts from './pages/posts';
+import Category from './pages/category';
 
 function App() {
 
@@ -16,8 +16,8 @@ function App() {
           <Route path="/" element={<Index />}/>
           <Route path="/category/:id" element={<Category />}/>
           <Route path="/posts" element={<Posts />}/>
-            <Route path="/post/:id" element={<Post />}/>
-            <Route path="/about_us" element={<AboutUs />}/>
+          <Route path="/post/:id" element={<Post />}/>
+          <Route path="/about_us" element={<AboutUs />}/>
         </Routes>
       </BrowserRouter>
     </>
