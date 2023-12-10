f23fce32faf1fe3240baeb663d3164161083aa56
Steven Joseph <stevolisisjoseph@gmail.com>
2023-12-10 01:02:19 +0100

    responsiveness update

--- src/components/motto.jsx
+++ src/components/motto.jsx
@@ -3,17 +3,17 @@ export default function Motto(){
 
     return(
         <section className="text-[#24247A] py-12 bg-white text-center overflow-x-hidden">
-            <div className="radial -ml-[20px] bg-[#6EACE6CC] w-[102vw] sm:w-[101vw] rotate-[-1.5deg] flex justify-center items-center flex-col">
-                <h3 className="text-lg sm:text-xl font-[Playfair] font-semibold w-[400px] sm:w-[500px] py-7">
+            <div className="px-12 radial -ml-[3px] sm:-ml-[20px] bg-[#6EACE6CC] w-[102vw] sm:w-[103vw] rotate-[-1.5deg] flex justify-center items-center flex-col">
+                <h3 className="text-lg sm:text-xl font-[Playfair] font-semibold w-[85vw] sm:w-[42vw] md:w-[35vw] py-7">
                     "Investing in real estate isn't just the best and fastest route to wealth—it's the safest and the only way."
                 </h3>
-                <p className="text-xs sm:text-sm font-[Raleway] pb-8 w-[370px] sm:w-[450px]">
+                <p className="text-xs sm:text-sm font-[Raleway] pb-8 w-[370px] sm:w-[60vw] md:w-[32vw]">
                     Discover furnished homes tailored to your taste with our diverse selection of spacious and comfortable living spaces.
                 </p>
             </div>
-            <div>
+            {/* <div>
                 <img src="/motto_ellipse"/>
-            </div>
+            </div> */}
         </section>
     )
 }
