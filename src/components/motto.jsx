f8536c9e0181bbb2a6802bff1d7e041e55252bb5
Steven Joseph <stevolisisjoseph@gmail.com>
2023-12-10 13:10:17 +0100

    gradient

--- src/components/motto.jsx
+++ src/components/motto.jsx
@@ -4,10 +4,10 @@ export default function Motto(){
     return(
         <section className="text-[#24247A] py-12 bg-white text-center overflow-x-hidden">
             <div className="px-12 radial -ml-[3px] sm:-ml-[20px] bg-[#6EACE6CC] w-[102vw] sm:w-[103vw] rotate-[-1.5deg] flex justify-center items-center flex-col">
-                <h3 className="text-lg sm:text-xl font-[Playfair] font-semibold w-[85vw] sm:w-[42vw] md:w-[35vw] py-7">
+                <h3 className="text-lg sm:text-xl font-[Playfair] font-bold w-[90vw] sm:w-[42vw] md:w-[35vw] py-7">
                     "Investing in real estate isn't just the best and fastest route to wealth—it's the safest and the only way."
                 </h3>
-                <p className="text-xs sm:text-sm font-[Raleway] pb-8 w-[370px] sm:w-[60vw] md:w-[32vw]">
+                <p className="text-xs sm:text-sm font-[Raleway] font-semibold pb-8 w-[82vw] sm:w-[60vw] md:w-[32vw]">
                     Discover furnished homes tailored to your taste with our diverse selection of spacious and comfortable living spaces.
                 </p>
             </div>
